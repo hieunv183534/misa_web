4f9e555e57db51b3feba0277b87709a206dc9df5
hieunv183534 <nvhieu@rd.local>
2021-07-14 15:57:38 +0700

    update table, profile-dialog

---
+++ 1.UI/js/page/employee.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    $(".employee-profile-dialog").hide();
+    $(".dialog-modal").hide();
+})
+
+
+$(".content .content-body .grid table tbody tr").click(function () {
+    $(".employee-profile-dialog").show();
+    $(".dialog-modal").show();
+});
+
+$(".btn-cancel").click(function () {
+    $(".employee-profile-dialog").hide();
+    $(".dialog-modal").hide();
+});
+
+$(".btn-save").click(function () {
+    $(".employee-profile-dialog").hide();
+    $(".dialog-modal").hide();
+});
+
+$(".employee-profile-dialog .dialog-header button.btn-exit").click(function () {
+    $(".employee-profile-dialog").hide();
+    $(".dialog-modal").hide();
+});
